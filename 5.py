8652e808a413a72c071aecf38f735918a53e5e7b
刘备 <admin@yitianyigexiangfa.com>
2017-11-16 21:23:27 +0800

    5

---
+++ 5.py
@@ -0,0 +1,16 @@
+import pickle
+
+src = open('banner.p', 'rb')
+result = pickle.load(src)
+
+def print_ary(ary, s):
+    for i in range(len(ary)):
+        element = ary[i]
+        s = ""
+        for i in range(len(element)):
+            sub_element = element[i]
+            s += sub_element[0] * sub_element[1]
+        print(s)
+
+print_ary(result, "")
+
