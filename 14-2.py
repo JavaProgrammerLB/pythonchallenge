a60c97ffab8f0c7cec73679bf33dd7eec6b071a5
刘备 <admin@yitianyigexiangfa.com>
2017-11-20 21:27:20 +0800

    start 14

---
+++ 14-2.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from PIL import ImageDraw
+
+
+im = Image.open("wire/wire.png")
+size = im.size
+width = size[0]
+height = size[1]
+count = 0
+for x in range(width):
+    for y in range(height):
+        print(im.getpixel((x, y)))
+        count += 1
+print(count)
