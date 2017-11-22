ac3d769b4d96e42bb1c5466bd7f9d65bb55a82b0
刘备 <admin@yitianyigexiangfa.com>
2017-11-22 22:02:00 +0800

    delete draft

--- 18.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    file1 = open("swan/left.patch", "r")
-    file2 = open("swan/liubei.txt", "w")
-    while True:
-        line = file1.readline()
-        if not line:
-            break
-        if line.startswith("+") or line.startswith("-"):
-            file2.write(line)
