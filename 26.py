a9438aa0516465254629e7afaf7ee7f9b897a2f3
刘备 <admin@yitianyigexiangfa.com>
2017-12-26 20:38:54 +0800

    try 26

---
+++ 26.py
@@ -0,0 +1,14 @@
+import hashlib
+
+
+def main():
+    file_path = "maze/maze/mybroken.zip"
+    first_step(file_path)
+
+
+def first_step(file_path):
+    file = open(file_path, "rb")
+
+
+if __name__ == "__main__":
+    main()
