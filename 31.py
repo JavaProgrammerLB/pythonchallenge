df9eda9238e7df2b74a8e1eb253e76ae5b55fd14
刘备 <admin@yitianyigexiangfa.com>
2018-07-09 10:50:00 +0800

    Revert "use np to solve equation"
    This reverts commit 115772a

--- 31.py
+++ 31.py
@@ -1,16 +1,10 @@
 from PIL import Image
 from PIL import ImageSequence
-import numpy as np
-import matplotlib.pyplot as plt
 
 
 def main():
     pic_path = "ufo/mandelbrot.gif"
-    # first_step(pic_path)
-    # second_step()
-    # plt.imshow(mandelbrot(1000, 1000))
-    # plt.show()
-    numPyVersion()
+    first_step(pic_path)
 
 
 def first_step(input):
@@ -24,43 +18,8 @@ def first_step(input):
     height = 480
     for x in range(width):
         for y in range(height):
-            print(im.getpixel((x, y)), end=" ")
+            print(im.getpixel((x, y)), end = " ")
         print()
-
-
-def second_step():
-    a = np.arange(15).reshape(3, 5)
-    print(a)
-    print(a.shape)
-    print(a.ndim)
-    print(a.dtype.name)
-    print(a.dtype)
-    print(a.itemsize)
-    print(type(a))
-
-    b = np.array([6, 7, 8])
-    print(b)
-    print(b.shape)
-    pass
-
-
-def mandelbrot(h, w, maxit=20):
-    y, x = np.ogrid[-1.4:1.4:h * 1j, -2:0.8:w * 1j]
-    c = x + y * 1j
-    z = c
-    divtime = maxit + np.zeros(z.shape, dtype=int)
-
-    for i in range(maxit):
-        z = z ** 2 + c
-        diverge = z * np.conj(z) > 2 ** 2
-        div_now = diverge & (divtime == maxit)
-        divtime[div_now] = i
-        z[diverge] = 2
-    return divtime
-
-
-def numPyVersion():
-    print(np.version.version)
 
 
 if __name__ == "__main__":
